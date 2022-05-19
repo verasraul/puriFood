21dde24eb5640f8606b57c8735ba24552dc182e7
Raul Veras <verasraul@gmail.com>
2022-05-18 20:22:38 -0400

    creating search input fields for client api search

---
+++ app/client/src/App.js
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <div className="App">
+      <h1><u>PuriFood</u></h1>
+
+      <div className="app-searchForm">
+        <input 
+        type="text" 
+        placeholder="Type the ingredient"
+        autoComplete="Off"
+        className="app-searchInput" 
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
