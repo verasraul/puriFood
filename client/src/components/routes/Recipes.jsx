282c78fb71f3d2615b42bc6de95cffe0ae9955e3
Raul Veras <verasraul@gmail.com>
2022-06-01 00:07:39 -0400

    working with ports to fix backend in heroku

--- client/src/components/routes/Recipes.jsx
+++ client/src/components/routes/Recipes.jsx
@@ -8,7 +8,7 @@ const Recipes = () => {
 
   const fetchData = async () => {
     try {
-      const res = await axios(`http://localhost:8000/api/recipes`);
+      const res = await axios(`http://localhost:3000/recipes`);
       setRecipes(res.data.MyRecipes);
     } catch (err) {
       console.log(err);
