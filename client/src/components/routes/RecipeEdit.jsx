de99345af2aa2ded6f4e4cb28382096b612ecfa9
Raul Veras <verasraul@gmail.com>
2022-06-01 00:11:35 -0400

    working with ports to fix backend in heroku

--- client/src/components/routes/RecipeEdit.jsx
+++ client/src/components/routes/RecipeEdit.jsx
@@ -15,7 +15,7 @@ function RecipeEdit() {
 
   const fetchData = async () => {
     try {
-      const response = await axios(`http://localhost:3000/recipes/${id}`);
+      const response = await axios(`http://localhost:3001/recipes/${id}`);
       console.log(response);
       setRecipe(response.data);
     } catch (error) {
@@ -35,7 +35,7 @@ function RecipeEdit() {
   const handleSubmit = (event) => {
     event.preventDefault();
     axios({
-      url: `http://localhost:3000/recipes/${id}/update-recipe`,
+      url: `http://localhost:3001/recipes/${id}/update-recipe`,
       method: "PUT",
       data: recipe,
     })
